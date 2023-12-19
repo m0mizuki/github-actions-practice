ede600f23d143b224129432a5e6e563ad58c5245
m0mizuki <mutou.m.2020@gmail.com>
2023-12-19 19:25:14 +0900

    feat: 環境変数を設定

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function setText(){
+    const button_0 = document.getElementById("button_0");
+    console.log(process.env.REACT_APP_SECRET_1);
+    console.log(process.env.REACT_APP_SECRET_2);
+    button_0.innerText=process.env.REACT_APP_SECRET_1;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <p>こんにちは</p>
+        <p>SECRET_1</p>
+        <p id="secret_1"></p>
+        <button id="button_0" onClick={()=>setText()}>あああ</button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
